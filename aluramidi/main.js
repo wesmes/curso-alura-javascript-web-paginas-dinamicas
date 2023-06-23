ad678bad0cecd10e9895258eb45e28027cc6be39
Weslley Gomes da Cruz <w_gomes@protonmail.com>
2023-06-23 19:53:41 -0300

    Módulo 5 - Aula 03: Adicionando e removendo classes

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -24,4 +24,8 @@ for(let contador=0;contador<listaDeTeclas.length;contador++)
         tecla.classList.add('ativa');
     }
 
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa');
+    }
+
 }
