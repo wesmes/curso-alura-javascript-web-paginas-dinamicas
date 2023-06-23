30ddf74ea4549c53b99802b8014d691f7cec14c5
Weslley Gomes da Cruz <w_gomes@protonmail.com>
2023-06-23 14:24:26 -0300

    Módulo 4 - Aula 2: Percorrendo uma lista

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -5,4 +5,10 @@ function tocaSomPom()
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+let contador = 0;
+
+while(contador < listaDeTeclas.length)
+{
+    listaDeTeclas[contador].onclick = tocaSomPom;
+    contador = contador+1;
+}
