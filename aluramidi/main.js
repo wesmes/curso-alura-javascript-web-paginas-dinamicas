44f75b22433201adec1e8549097c45ed0ee75261
Weslley Gomes da Cruz <w_gomes@protonmail.com>
2023-06-23 20:17:49 -0300

    Módulo 5 - Aula 06: Operador lógico

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -19,10 +19,11 @@ for(let contador=0;contador<listaDeTeclas.length;contador++)
     }
 
     tecla.onkeydown = function(evento) {
-        if(evento.code === 'Space')
-        {
+
+        if(evento.code === 'Space' || evento.code === 'Enter') {
             tecla.classList.add('ativa');
         }
+
     }
 
     tecla.onkeyup = function() {
