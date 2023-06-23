dc35d54c8528197c9c9f18cef4643f7456ca2f06
Weslley Gomes da Cruz <w_gomes@protonmail.com>
2023-06-23 17:05:51 -0300

    Módulo 4 - Aula 07: Textos dinâmicos

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -10,8 +10,14 @@ let contador = 0;
 while(contador < listaDeTeclas.length)
 {
     
-    listaDeTeclas[contador].onclick = function(){
-        tocaSom('#som_tecla_pom');
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    // template string
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function(){
+        tocaSom(idAudio);
     }
 
     contador = contador+1;
